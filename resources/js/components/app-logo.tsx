3c5248b21d1e731e5a0340faa46a13ae20ad3055
Sirotkin <nikolay.sirotkin@yvl.local>
2025-11-26 10:42:40 +0300

    Change logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,13 +2,13 @@ import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
-        <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+        <div className="flex items-center space-x-1">
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">
+                <AppLogoIcon className="size-8 fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+            <div className="ml-1 grid flex-1 pt-1 text-left">
+                <span className="text-lg leading-tight font-semibold">Язаки Волга</span>
             </div>
-        </>
+        </div>
     );
 }
